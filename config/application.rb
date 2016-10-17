c1afb3b5b1b09faac973be7857f69780e9eec365
audreybench3 <audrey.benchaya@gmail.com>
2016-10-17 21:29:59 +0200

    cross domain

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
     
 module Audreys7
   class Application < Rails::Application
+    
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
